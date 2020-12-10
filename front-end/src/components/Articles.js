2bbfa02baf755510549841318e991b241ab0f8c3
your_github_ <your_email@example.com>
2020-12-10 22:46:19 +0200

    Project react level 2

--- front-end/src/components/Articles.js
+++ front-end/src/components/Articles.js
@@ -1,10 +1,10 @@
-import React, { useState,useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Articles=()=> {
+const Articles = () => {
   // [nameOfTheState,functionToChangeTheState] = (initalValue)
   const [articles, setArticles] = useState([]);
-  const [id,SetID]=useState(0);
+  const [id, SetID] = useState(0);
   const [Title, setNewTitle] = useState("");
   const [description, setNewDesc] = useState("");
   const [Author, setNewAuthor] = useState("");
@@ -16,21 +16,21 @@ const Articles=()=> {
     axios
       .get(`http://localhost:5000/articlesMySql`)
       .then((response) => {
-        console.log('RESPONSE: ', response);
-        console.log('DATA: ', response.data);
+        console.log("RESPONSE: ", response);
+        console.log("DATA: ", response.data);
         setArticles(response.data);
       })
       .catch((err) => {
-        console.log('ERR: ', err);
+        console.log("ERR: ", err);
       });
   };
- 
+
   const addNewArticle = () => {
     axios
       .post(`http://localhost:5000/articles/createMySql`, {
-        title:Title,
-        description:description,
-        author:Author,
+        title: Title,
+        description: description,
+        author: Author,
       })
       .then((response) => {
         console.log("RESPONSE: ", response);
@@ -43,42 +43,47 @@ const Articles=()=> {
         console.log("ERR: ", err);
       });
   };
-  const deleteThisArticle=()=>{
+  const deleteThisArticle = () => {
     axios
       .delete(`http://localhost:5000/articlesMySql/${id}`)
       .then((response) => {
-        console.log('DATA: ', response.data);
-        if(response.status === 200){
-          
-          
+        console.log("DATA: ", response.data);
+        if (response.status === 200) {
         }
       })
       .catch((err) => {
-        console.log('ERR: ', err);
+        console.log("ERR: ", err);
       });
-  }
+  };
 
-  const NewArticle = articles.map((e,i)=>{
-    return<div num={i +1} Key={i}>
-      <div className='card'>
-        <div class="card-body">
-      <h4 class="card-title">Title:{e.Title}</h4>
-      <p class="card-text">Description:{e.description}</p>
-      <div class="card-text">Author:{e.Author}</div>
-      
+  const NewArticle = articles.map((e, i) => {
+    return (
+      <div num={i + 1} Key={i}>
+        <div className="card">
+          <div class="card-body">
+            <h4 class="card-title">Title:{e.Title}</h4>
+            <p class="card-text">Description:{e.description}</p>
+            <div class="card-text">Author:{e.Author}</div>
+          </div>
+        </div>
       </div>
-      </div>
-    </div>
-  })
+    );
+  });
 
   return (
     <div className="app">
       <div className="search">
-        <input type="search" placeholder="Search" className="form-control"></input>
-        <button type="submit" className="btn btn-primary">Search</button>
+        <input
+          type="search"
+          placeholder="Search"
+          className="form-control"
+        ></input>
+        <button type="submit" className="btn btn-primary">
+          Search
+        </button>
       </div>
-      
-        <form onClick={addNewArticle} className="NewItem">
+
+      <form onClick={addNewArticle} className="NewItem">
         <input
           type="text"
           className="form-control mb-2 mr-sm-2"
@@ -106,17 +111,15 @@ const Articles=()=> {
             setNewAuthor(e.target.value);
           }}
         />
-        <button
-          type="submit"
-          className="btn btn-danger mb-2"
-        >
+        <button type="submit" className="btn btn-danger mb-2">
           Add
         </button>
-        </form>
-        <div className="border">
-          {NewArticle}
-        </div>
+      </form>
+
+      <div class="container">
+        <div class="row row-cols-5">{NewArticle}</div>
+      </div>
     </div>
   );
-}
+};
 export default Articles;
